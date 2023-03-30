b64a4e7ab8148ec482c4e14161edffa042263204
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-03-30 11:21:11 +0200

    Update namehasher.py

--- Arch2/namehasher.py
+++ Arch2/namehasher.py
@@ -90,7 +90,8 @@ def main():
     else:
         key = 'a_b?c9d6e1f4g!h:i<j|k{l0m@n7o+p~q2r+s/t=u^v3w]x(y-z>A*B8C;D%E#F}G5H)I[J$'
     move = ''
-    if key == False:
+    check = set_hashmap(key)
+    if check == False:
         move = 'Q'
     while move != 'Q':
         move = input('''[E] Encode value to hashed value
