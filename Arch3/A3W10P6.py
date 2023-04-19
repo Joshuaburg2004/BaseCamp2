3c57391ccca47dce8fbf527f0e643a7ecb9c9178
Joshuaburg2004 <127229685+Joshuaburg2004@users.noreply.github.com>
2023-04-19 13:47:24 +0200

    Add files via upload

---
+++ Arch3/A3W10P6.py
@@ -0,0 +1,38 @@
+def scrub(ine):
+    res = []
+    try:
+        with open(ine, 'r') as f:
+            prevline = ''
+            num = 1
+            for line in f:
+                aline = line.strip().split()
+                if len(aline) > 1:
+                    if aline[0] == 'def':
+                        if prevline != '':
+                            if prevline[0] != '#':
+                                aline[1] = aline[1].strip(':')
+                                res.append(f'File: {ine} contains a function [{aline[1]}] on line [{num}]\
+ without preceding comment.')
+                        else:
+                            aline[1] = aline[1].strip(':')
+                            res.append(f'File: {ine} contains a function [{aline[1]}] on line [{num}]\
+ without preceding comment.')
+                prevline = line
+                num += 1
+            return res
+    except FileNotFoundError:
+        return False
+
+
+def main():
+    ine = input('File to read: ').split(', ')
+    for i in ine:
+        content = scrub(i)
+        if content is False:
+            print('Error')
+        for res in content:
+            print(res)
+
+
+if __name__ == '__main__':
+    main()
